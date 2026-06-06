4399a80451339f0d6bafb2717e72111bd803432a
Andka670 <m.andhikaa2018@gmail.com>
2026-06-06 12:43:01 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://azynaphouwbefefmnrcc.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_Rdhj1cSJgMhzWUdYNLmSPA_BK-DdlEd";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6eW5hcGhvdXdiZWZlZm1ucmNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMjU0ODksImV4cCI6MjA5NTcwMTQ4OX0.Oy1vXdxQi9gBnfEtotFcAMkeHXyHFk2gS8BUFdrz4fI";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
